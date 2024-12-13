4328eb7696990d9d74878c11632c3e967806bfd3
Ye-Buehler <ye.buehler-feng@unibe.ch>
2024-12-13 23:09:32 +0100

    simplify FG result

--- src/gdb_ml/properties_calculator.py
+++ src/gdb_ml/properties_calculator.py
@@ -528,7 +528,7 @@ class PropertiesCalculator:
         elif filter2 < 0:
             pass_check = False
 
-        return [pass_check, [filter1, filter2, filter3, filter4, filter5, filter6, filter7, filter8, filter9, filter10]]
+        return pass_check
 
 
 
